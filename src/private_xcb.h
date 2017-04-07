7b320ac8da17470022de7a8943531ad3a27174f5
Ivan Krylov <krylov.r00t@gmail.com>
2017-04-07 10:11:41 +0300

    dammit, I need to store atoms somewhere, too

--- src/private_xcb.h
+++ src/private_xcb.h
@@ -2,5 +2,10 @@
 
 struct ForeignWindowImpl {
 	xcb_window_t wid;
+	xcb_connection_t* conn;
 };
 
+struct XCB_atoms {
+	static xcb_atom_t NET_ACTIVE_WINDOW;
+	static xcb_atom_t NET_WM_NAME;
+};
---
+++ src/window_watcher.cpp
@@ -0,0 +1,2 @@
+xcb_atom_t XCB_atoms::NET_ACTIVE_WINDOW;
+xcb_atom_t XCB_atoms::NET_WM_NAME;
