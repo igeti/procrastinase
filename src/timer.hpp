c3d8cfe98cd730fcc791859eef08f1ec32ba2e21
aitap <krylov.r00t@gmail.com>
2017-04-09 22:05:29 +0300

    timer: destructor and most of the structure is ready, only the code remains to be written

--- src/timer.hpp
+++ src/timer.hpp
@@ -14,31 +14,57 @@ public:
 	timer& add_rel(std::chrono::steady_clock::duration diff, T cb) {
 		return add_abs(std::chrono::steady_clock::now() + diff, cb);
 	}
-	timer& add_abs(std::chrono::steady_clock::time_point when, T cb) {}
-	thr_timer() : waiter(loop, this) {
+	timer& add_abs(std::chrono::steady_clock::time_point when, T cb) {
 		/*
-		 * Keep it locked for most of the time.
 		 * After changing timers,
-		 * unlock it,
+		 * recalculate next_wakeup,
 		 * set changes_pending = true,
 		 * wait on change_noted while changes_pending.
 		 */
-		lock.lock();
+	}
+	thr_timer()
+		: lock(mutex), changes_pending(false), terminating(false),
+		next_wakeup(std::chrono::steady_clock::now()), waiter(loop, this, next_wakeup) {}
+	~thr_timer() {
+		terminating = true;
+		changes_pending = true;
+		while (changes_pending) change_noted.wait(lock);
+		waiter.detach();
 	}
 private:
-	std::timed_mutex lock;
+	std::timed_mutex mutex;
+	std::unique_lock<std::timed_mutex> lock; // keep mutex locked by owning thread most of the time
 	bool changes_pending;
+	bool terminating;
+	std::chrono::steady_clock::time_point next_wakeup;
 	std::condition_variable change_noted;
 	std::list<timer> timers;
 	std::thread waiter;
-	void loop() {
+	void loop(std::chrono::steady_clock::time_point wakeup) {
+		for(/**/;;/**/) { // <-- behold the ASCII Cthulhu
 		/*
 		 * Most of the time, keep trying to unlock the mutex with timeout
 		 * set to the closest timer expiration.
-		 * If succeeded, some change has occured and we need to recalculate
-		 * the timeout, set changes_pending=false and fire change_noted.
-		 * If failed, timer has fired and we need to call the callback.
 		 */
+		// wakeup in the past means that there are no timers set => just keep waiting
+			if (
+				std::chrono::steady_clock::now() > wakeup
+				? mutex.try_lock_until(wakeup)
+				: mutex.try_lock()
+			) {
+				/*
+				 * If succeeded, some change has occured and we need to record
+				 * the new timeout, set changes_pending=false, fire change_noted,
+				 * then resume locking. If terminating, return.
+				 */
+				 // don't forget
+				 mutex.unlock();
+			} else {
+			/*
+			 * If failed, timer has fired and we need to call the callback.
+			 */
+			}
+		}
 	}
 };
 
