9c3a1042a41df4f22ff461266a37935a7757abc1
Ivan Krylov <krylov.r00t@gmail.com>
2017-04-10 10:13:25 +0300

    timer: has lots of strange bugs, not useable yet

--- src/timer.hpp
+++ src/timer.hpp
@@ -10,21 +10,26 @@ template <class T>
 class thr_timer {
 public:
 	class timer {
-		std::chrono::steady_clock::duration time;
+		friend class thr_timer;
+		std::chrono::steady_clock::time_point time;
 		T & cb;
-		timer(std::chrono::steady_clock::duration time_, T & cb_) : time(time_), cb(cb_) {}
-		void cancel() { throw "TODO: unimplemented"; }
+		timer(std::chrono::steady_clock::time_point time_, T & cb_)
+			: time(time_), cb(cb_) {}
+	public:
+		bool operator==(const timer& other) {
+			return (time == other.time) && (&cb == &other.cb);
+		}
 	};
-	timer & add_rel(std::chrono::steady_clock::duration diff, T & cb) {
+	timer * add_rel(std::chrono::steady_clock::duration diff, T & cb) {
 		return add_abs(std::chrono::steady_clock::now() + diff, cb);
 	}
-	timer& add_abs(std::chrono::steady_clock::time_point when, T & cb) {
+	timer * add_abs(std::chrono::steady_clock::time_point when, T & cb) {
+		timer * ret = nullptr;
 		{
-			std::unique_lock<std::mutex> lock(list_mutex); // list access is additionally guarded
+			std::unique_lock<std::mutex> lock(list_mutex);
 			timers.push_back({when, cb});
-			next_wakeup = timers.front().when;
-			for (timer& t: timers)
-				if (t.when < next_wakeup) next_wakeup = t.when;
+			recalculate_wakeup();
+			ret = &timers.back();
 		}
 		/*
 		 * After changing timers,
@@ -34,10 +39,18 @@ public:
 		 */
 		changes_pending = true;
 		while (changes_pending) change_noted.wait(lock);
+		return ret;
+	}
+	void cancel(timer ** t) {
+		assert(t);
+		std::unique_lock<std::mutex> lock(list_mutex);
+		timers.remove(**t);
+		*t = nullptr;
+		recalculate_wakeup();
 	}
 	thr_timer()
 		: lock(tmutex), changes_pending(false), terminating(false),
-		next_wakeup(std::chrono::steady_clock::now()), waiter(loop, this, next_wakeup) {}
+		next_wakeup(std::chrono::steady_clock::now()), waiter(&thr_timer::loop, this, next_wakeup) {}
 	~thr_timer() {
 		terminating = true;
 		changes_pending = true;
@@ -46,27 +59,37 @@ public:
 	}
 private:
 	std::timed_mutex tmutex;
-	std::mutex list_mutex;
+	std::mutex list_mutex; // list access is additionally guarded
 	std::unique_lock<std::timed_mutex> lock; // keep mutex locked by owning thread most of the time
 	bool changes_pending;
 	bool terminating;
 	std::chrono::steady_clock::time_point next_wakeup;
-	std::condition_variable change_noted;
+	std::condition_variable_any change_noted;
 	std::list<timer> timers;
 	std::thread waiter;
+	void recalculate_wakeup() {
+		if (timers.empty()) return;
+		next_wakeup = timers.front().time;
+		for (timer& t: timers)
+			if (t.time < next_wakeup) next_wakeup = t.time;
+	}
 	void loop(std::chrono::steady_clock::time_point wakeup) {
 		for(/**/;;/**/) { // <-- behold the ASCII Cthulhu
-		/*
-		 * Most of the time, keep trying to unlock the mutex with timeout
-		 * set to the closest timer expiration.
-		 */
-		std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
-		// wakeup in the past means that there are no timers set => just keep waiting
-			if (
-				now > wakeup
-				? tmutex.try_lock_until(wakeup)
-				: tmutex.try_lock()
-			) {
+			/*
+			 * Most of the time, keep trying to unlock the mutex with timeout
+			 * set to the closest timer expiration.
+			 */
+			std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+			bool locked = false;
+			// wakeup in the past means that there are no timers set => just keep waiting until something changes
+			if (now < wakeup) {
+				locked = tmutex.try_lock_until(wakeup);
+			} else {
+				tmutex.lock();
+				locked = true;
+			}
+			now = std::chrono::steady_clock::now(); // it might have taken a lot of time to get there
+			if (locked) {
 				/*
 				 * If succeeded, some change has occured and we need to record
 				 * the new timeout, set changes_pending=false, fire change_noted,
@@ -87,15 +110,21 @@ private:
 			 */
 			{
 				std::unique_lock<std::mutex> lock(list_mutex);
+				now = std::chrono::steady_clock::now(); // and maybe here, too
 				if (timers.empty()) continue;
-				while (
+				for (
 					auto t = std::find_if(
 						timers.begin(), timers.end(),
-						[now](timer&t){ t.time <= now; }
-					) != timers.end()
+						[now](timer&t){ return t.time <= now; }
+					)
+					;t != timers.end();
+					t = std::find_if(
+						timers.begin(), timers.end(),
+						[now](timer&t){ return t.time <= now; }
+					)
 				) {
 					t->cb();
-					t.erase(t);
+					timers.erase(t);
 				}
 			}
 		}
