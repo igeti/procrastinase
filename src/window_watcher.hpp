8cc9893696439f70ab0bb037f466f552d7bd796d
Ivan Krylov <krylov.r00t@gmail.com>
2017-04-14 13:05:38 +0300

    WindowWatcher: we'll need the queue and the timer in the class for all implementations

--- src/window_watcher.hpp
+++ src/window_watcher.hpp
@@ -1,6 +1,23 @@
 #include "foreign_window.hpp"
+#include "queue.hpp"
+#include "timer.hpp"
 
 struct WindowWatcherImpl;
+
+struct WindowEvent {
+	enum class WindowEventType {
+		new_active,
+		new_title,
+		alarm_timer,
+		kill_timer
+	} type;
+	ForeignWindow wnd;
+	std::string title;
+	WindowEvent(WindowEventType t, ForeignWindow && w)
+		: type(t), wnd(std::move(w)) {}
+	WindowEvent(std::string t, ForeignWindow && w)
+		: type(WindowEventType::new_title), wnd(std::move(w)), title(t) {}
+};
 
 class WindowWatcher {
 public:
@@ -12,5 +29,7 @@ private:
 	double work_to_play;
 	int play_credit_left_ms; // QTimer accepts int
 	// TODO: alarm sound object & program list object
+	thr_queue<WindowEvent> messages;
+	thr_timer<void (WindowWatcher::*)(const ForeignWindow&)> tmr;
 	std::unique_ptr<WindowWatcherImpl> impl;
 };
