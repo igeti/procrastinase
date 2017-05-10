694553f6c9d557542e3a211b93c019742c5fde48
Ivan Krylov <krylov.r00t@gmail.com>
2017-05-10 20:11:16 +0300

    ProgramList: implement constructor

--- src/program_list.cpp
+++ src/program_list.cpp
@@ -1,6 +1,9 @@
 #include "program_list.hpp"
 
-ProgramList::satisfies(const ForeignWindow & wnd) {
+ProgramList::ProgramList(const std::set<std::string> & paths_, const std::set<std::string> & substrings)
+	: paths(paths_), title_substrings(substrings) {}
+
+bool ProgramList::satisfies(const ForeignWindow & wnd) {
 	std::lock_guard<std::mutex> lock(mutex);
 	if (paths.count(wnd.get_program_path())) return true;
 	std::string title = wnd.get_window_title();
@@ -8,4 +11,5 @@ ProgramList::satisfies(const ForeignWindow & wnd) {
 		if (title.find(substr) != std::string::npos) return true;
 	return false;
 }
-		
+
+
