b6ea09b805f07647c4c2939a784589e808692303
Ivan Krylov <krylov.r00t@gmail.com>
2017-05-10 19:26:45 +0300

    ProgramList: better interface, no inheritance needed

--- src/program_list.hpp
+++ src/program_list.hpp
@@ -7,22 +7,9 @@
 class ProgramList {
 public:
 	const ProgramRole type;
-	virtual bool satisfies(const ForeignWindow&) = 0;
-	virtual ~ProgramList();
-};
-
-class ProgramPathSet : public ProgramList {
-public:
-	ProgramPathSet(const std::set<std::string>&);
-	bool satisfies(const ForeignWindow&) override;
+	bool satisfies(const ForeignWindow&);
+	~ProgramList();
 private:
 	std::set<std::string> paths;
-};
-
-class WindowTitleSubstringList : public ProgramList {
-public:
-	WindowTitleSubstringList(const std::list<std::string>&);
-	bool satisfies(const ForeignWindow&) override;
-private:
 	std::list<std::string> title_substrings;
 };
