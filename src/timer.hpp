77c16adfdd1f4b017e30fbf609c23fc37abb110e
aitap <krylov.r00t@gmail.com>
2017-05-23 21:04:22 +0300

    More comments for timer.hpp => improve the bus factor
    TODO: it's not a template class anymore => maybe split off timer.cpp?

--- src/timer.hpp
+++ src/timer.hpp
@@ -9,13 +9,13 @@
 /*
 Usage:
 
-thr_timer<callback_type> tmr;
-
-Use tmr.add_rel or tmr.add_abs to add timers and remember their return values.
-Use tmr.cancel(timer) to cancel existing timers. After cancellation, you can
+1) Create a thr_timer tmr;
+2) Use tmr.add_rel or tmr.add_abs to add timers and remember their return values.
+Don't feed temporary cb objects because timer only remembers pointers to them.
+3) Use tmr.cancel(timer) to cancel existing timers, if you need to. After cancellation, you can
 assign a different timer to the same variable. Use operator bool overload to
-check if a given timer is active. Cancelling a dead timer is no-op.
-Once thr_timer moves out of scope, it stops the watcher threads and cancels
+check if a given timer is active (not cancelled yet). Cancelling a dead timer is no-op.
+4) Once thr_timer moves out of scope, it stops the watcher threads and cancels
 the remaining timers.
 */
 
@@ -53,6 +53,7 @@ public:
 		 * wait on change_noted while changes_pending.
 		 */
 		changes_pending = true;
+		// wake up the waiting thread so it would set the appropriate timeout
 		while (changes_pending) change_noted.wait(lock);
 		// return a copy because queue+vector invalidates pointers/references on a whim
 		return {when, &cb};
@@ -62,6 +63,8 @@ public:
 		std::unique_lock<std::mutex> lock(list_mutex);
 		timers.remove(t);
 		t.cb = nullptr;
+		// no need to wake up the waiting thread specifically: it'll just note that
+		// the stars aren't right yet and go back to sleep
 	}
 	thr_timer()
 		: lock(tmutex), changes_pending(false), terminating(false),
@@ -82,7 +85,18 @@ private:
 			c.erase(std::remove(c.begin(), c.end(), val), c.end());
 		}
 	};
+	/*
+	How It Works:
+	This class abuses the ability of timed_mutex to wait for an opening to lock it.
+	By setting a timeout for the next timer and trying to lock this mutex, it's
+	able to be interrupted by the main thread releasing the lock.
+	*/
 	std::timed_mutex tmutex;
+	/*
+	Problem: most of the interesting stuff happens with tmutex owned by main thread
+	(on lock timeout)
+	Solution: guard the timer queue with another mutex
+	*/
 	std::mutex list_mutex; // list access is additionally guarded
 	std::unique_lock<std::timed_mutex> lock; // keep mutex locked by owning thread most of the time
 	bool changes_pending;
@@ -100,17 +114,26 @@ private:
 			bool wait_indefinite = true;
 			{
 				std::unique_lock<std::mutex> lock(list_mutex);
-				wait_indefinite = timers.empty();
+				wait_indefinite = timers.empty(); // no reason to wake up is there're no timers to fire
 				if (!wait_indefinite)
 					wait_until = timers.top().time;
 			}
 			bool locked = false;
+			// now wait for next timer to fire / any change in the queue
 			if (wait_indefinite) {
 				tmutex.lock();
 				locked = true;
 			} else {
 				locked = tmutex.try_lock_until(wait_until);
 			}
+			/*
+			Note: we have to use this synchro stuff for 2 reasons:
+			- mutexes issue memory barriers meaning that protected variables
+			are guaranteed to have consistent values in caches of different CPUs
+			- main thread needs to know when to lock the timed mutex again
+			- this also means that I can't put the `bool terminating` to the while clause:
+			it can be set from another thread, so no access before locking mutex
+			*/
 			if (locked) {
 				/*
 				 * Lock succeeded: maybe we're terminating?
@@ -125,6 +148,8 @@ private:
 			 * Whatever happens, check the queue for expired timers.
 			 */
 			{
+				// now that we access the timer queue, lock the appropriate mutex
+				// (not the timed one we've been abusing for side effects)
 				std::unique_lock<std::mutex> lock(list_mutex);
 				while (
 					!timers.empty()
